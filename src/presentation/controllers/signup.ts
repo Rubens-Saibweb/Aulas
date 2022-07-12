aa6ce333c5a47f0a3b3afdd2f8d252a7282bcfd4
Rubens Diego <Rubens Diego>
2022-07-11 22:19:16 -0300

    refactory: Simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,12 +5,22 @@ import { HttpRequest, HttpResponse } from "../protocols/http"
 export class SingUpController {
     handle(httprequest: HttpRequest): HttpResponse {
 
-        if (!httprequest.body?.name) {
-            return badrequest(new MissingParamsError('name'))
+        // if (!httprequest.body?.name) {
+        //     return badrequest(new MissingParamsError('name'))
+
+        // }
+        // if (!httprequest.body?.email) {
+        //     return badrequest(new MissingParamsError('email'))
+        // }
+
+        const requiredFields = ["name", "email"]
+        for (const field of requiredFields) {
+            if (!httprequest.body[field]) {
+                return badrequest(new MissingParamsError(field))
+            }
 
         }
-        if (!httprequest.body?.email) {
-            return badrequest(new MissingParamsError('email'))
-        }
+
+
     }
 }
